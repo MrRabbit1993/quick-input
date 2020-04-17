d0c9f218d1c18c5b0fc9319d73916614d1ed7206
兔子先生 <1993iweb@gmail.com>
2020-04-17 20:54:25 +0800

    配置props

--- examples/App.vue
+++ examples/App.vue
@@ -7,9 +7,7 @@ export default {
   render(h) {
     return (
       <div>
-        <div>-------------组件开始11111---------------</div>
         <quick-input/>
-        <div>---------------结束111222----------------</div>
       </div>
     );
   }
--- packages/input/src/index.vue
+++ packages/input/src/index.vue
@@ -1,15 +1,59 @@
 <script>
-export default{
-    name:"quick-input",
-    data(){
-        return{
-
-        }
-    },
-    render(h){
-        return(
-            <div>hello</div>
-        )
+export default {
+  name: "quick-input",
+  props: {
+    value: { type: [Number, String, Object] },
+    text: String,
+    selectable: { type: [Boolean, String], default: false },
+    selectFilter: { type: String, default: "*" },
+    filterName: { type: String, default: "filter" },
+    scope: { type: [String, Boolean, Number, Object, Array, Function] },
+    customOptions: { type: [Function, Array, Object] },
+    url: { type: String },
+    textUrl: { type: String },
+    placeholder: { type: String },
+    readonly: { type: [String, Boolean] },
+    disabled: { type: [String, Boolean] },
+    optionValue: { type: String, default: "value" },
+    optionLabel: { type: String, default: "label" },
+    suggestionDelay: { type: Number, default: 400 },
+    clearable: { type: [Boolean, String], default: true },
+    valueDetectOnChanged: { type: Boolean, default: false } // 是否在changed事件时检测 value == this.value
+  },
+  data() {
+    return {
+      dropdownTimer: null,
+      boxWidth: this.width,
+      loading: false,
+      focus: false,
+      hiddenValue: this.value,
+      displayText: this.text,
+      showSuggestions: false,
+      typing: true, // 手工输入标志
+      options: []
+    };
+  },
+  created() {
+    if (!this.customOptions && !this.url) {
+      throw new Error("请指定 customOptions 或 url 属性.");
     }
-}
+  },
+  render(h) {
+    return (
+      <div
+        class="qib-box-container"
+        title={this.displayText}
+        width={this.boxWidth}
+        ref="container"
+        onMouseover={() => (this.focus = true)}
+        onMouseout={() => (this.focus = false)}
+      >demo</div>
+    );
+  }
+};
 </script>
+<style lang="less" scoped>
+.qib-box-container {
+  display: inline-block;
+}
+</style>
